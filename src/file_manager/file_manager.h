eb88d5ea068716ebdae304d6e9be5eda6303ee26
Shredemption <stijnmichiels2001@gmail.com>
2024-12-04 21:01:50 +0100

    Improved Filemanager

--- src/file_manager/file_manager.h
+++ src/file_manager/file_manager.h
@@ -2,14 +2,51 @@
 #define FILE_MANAGER_H
 
 #include <string>
+#include <cstdlib>
 
 class FileManager
 {
+private:
+    typedef std::string (*Builder)(const std::string &path);
+
 public:
     FileManager();
     ~FileManager();
 
     static std::string read(const std::string &filename);
+
+    static std::string getPath(const std::string &path)
+    {
+        static std::string (*pathbuilder)(std::string const &) = getPathBuilder();
+        return (*pathbuilder)(path);
+    }
+
+private:
+    static std::string const &getRoot()
+    {
+        static char const *envRoot = getenv("LOGL_ROOT_PATH");
+        static char const *givenRoot = (envRoot != nullptr ? envRoot : "..");
+        static std::string root = (givenRoot != nullptr ? givenRoot : "");
+        return root;
+    }
+
+    static Builder getPathBuilder()
+    {
+        if (getRoot() != "")
+            return &FileManager::getPathRelativeRoot;
+        else
+            return &FileManager::getPathRelativeBinary;
+    }
+
+    static std::string getPathRelativeRoot(const std::string &path)
+    {
+        return getRoot() + std::string("/") + path;
+    }
+
+    static std::string getPathRelativeBinary(const std::string &path)
+    {
+        return "../../../" + path;
+    }
 };
 
 #endif // FILE_MANAGER_H
