5ab14eaf646e91589c90046facab7016dc91a93f
Shredemption <stijnmichiels2001@gmail.com>
2024-12-04 15:46:32 +0100

    Remove triangle code

--- src/main.cpp
+++ src/main.cpp
@@ -85,56 +85,6 @@ void processInput(GLFWwindow *window)
         cameraPosition -= cameraSpeed * worldUp;
 }
 
-void createTriangle(unsigned int &vao, unsigned int &vbo, unsigned int &ebo)
-{
-    // Define vertices and their colors
-    // Vertex has [x, y, z] and [r, g, b]
-    float triangleVertices[] = {
-        0.0f, 1.0f, 0.0f,   // pos1
-        1.0f, 0.0f, 0.0f,   // col1
-        -1.0f, -0.5f, 0.0f, // pos2
-        0.0f, 1.0f, 0.0f,   // col2
-        1.0f, -0.5, 0.0f,   // pos3
-        0.0f, 0.0f, 1.0f    // col3
-
-    };
-
-    // Define incides that represent vertex order
-    unsigned int triangleIndices[] = {
-        0, 1, 2};
-
-    // Generate VertexArrayObject, VertexBufferObject, ElementBufferObject to manage and store OpenGL state
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
-
-    // Bind VAO to start recording OpenGL state for triangle
-    glBindVertexArray(vao);
-
-    // Bind the VBO to the GL_ARRAY_BUFFER target to store vertex data
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // Upload vertex data to GPU
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
-
-    // Bind the EBO to the GL_ELEMENT_ARRAY_BUFFER target to store index data
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    // Upload index data to GPU
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangleIndices), triangleIndices, GL_STATIC_DRAW);
-
-    // Specify layout of vertex data:
-    // Attribute 0: Position [x, y, z] - 3 floats, starting at offset 0
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0); // Enable attr 0
-
-    // Attribute 1: Color [r, g, b] - 3 floats, starting at offset 3 * sizeof(float)
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1); // Enable attr 1
-
-    // Unbind the VBO, EBO and VAO (optional, to prevent accidental modifications of their data)
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // (VBO) EBO
-    glBindVertexArray(0);             // VAO
-}
-
 void createCube(unsigned int &vao, unsigned int &vbo, unsigned int &ebo)
 {
     // Define vertices and their colors
@@ -249,9 +199,8 @@ int main()
         return -1;
     }
 
-    // Create triangle
+    // Create object
     unsigned int vao, vbo, ebo;
-    // createTriangle(vao, vbo, ebo);
     createCube(vao, vbo, ebo);
 
     // Initialize shader
@@ -303,14 +252,8 @@ int main()
         deltaTime = time - lastTime;
         lastTime = time;
 
-        // Vary color based on time
-        float red = (std::sin(time * 0.5f) + 1.0f) / 2.0f;
-        float green = (std::sin(time * 0.3f) + 1.0f) / 2.0f;
-        float blue = (std::sin(time * 0.7f) + 1.0f) / 2.0f;
-
         // Clear color buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // glClearColor(red, green, blue, 1.0f);
 
         // Process Inputs
         processInput(window);
@@ -334,10 +277,6 @@ int main()
             // Bind the VAO that stores the triangles vertex data and settings
             glBindVertexArray(vao);
 
-            // Issue the draw command
-            // glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0); // Cube
-            // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0); // Triangle
-            
             // Draw multiple cubes
             std::vector<glm::vec3> positions = {
                 {0.0f, 0.0f, 0.0f},
@@ -377,7 +316,7 @@ int main()
         glfwPollEvents();
     }
 
-    // Cleanup traingle
+    // Cleanup buffers
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
     glDeleteBuffers(1, &ebo);
