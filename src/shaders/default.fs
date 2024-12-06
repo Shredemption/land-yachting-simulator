16490c8a4c48d3a35035f5f3b4eec2380b4de89d
Shredemption <stijnmichiels2001@gmail.com>
2024-12-07 00:10:02 +0100

    Diffuse lighting

--- src/shaders/default.fs
+++ src/shaders/default.fs
@@ -1,10 +1,14 @@
 #version 410 core
 out vec4 FragColor;
 
+in vec3 Normal;
 in vec2 TexCoords;
+in vec3 FragPos;
 
 uniform sampler2D texture_diffuse1;
 uniform vec3 ambientLightColor;
+uniform vec3 diffuseLightColor;
+uniform vec3 lightPos;
 
 void main() 
 {
@@ -14,6 +18,14 @@ void main()
     // Apply ambient light
     vec3 ambient = ambientLightColor * textureColor;
 
+    // Calculate diffuse lighting
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos-FragPos);
+
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * diffuseLightColor;
+
+    // vec3 result = (ambient + diffuse) * textureColor;
     vec3 result = ambient * textureColor;
     FragColor = vec4(result, 1.0f);
 }
--- src/shaders/default.vs
+++ src/shaders/default.vs
@@ -1,8 +1,11 @@
 #version 410 core
 layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aTexCoords;
 
+out vec3 Normal;
 out vec2 TexCoords;
+out vec3 FragPos;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
@@ -12,4 +15,6 @@ void main()
 {
     TexCoords = aTexCoords;
     gl_Position = u_projection * u_view * u_model * vec4(aPos, 1.0);
+    FragPos = vec3(u_model * vec4(aPos, 1.0));
+    Normal = aNormal;
 }
