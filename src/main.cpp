3bc7f48bcb5a8dfca0743036c08fddb8c937b3f7
Shredemption <stijnmichiels2001@gmail.com>
2024-12-06 23:08:18 +0100

    Enable Vsync

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_FOCUS_ON_SHOW, GL_TRUE);
+    glfwWindowHint(GLFW_REFRESH_RATE, GLFW_DONT_CARE);
 
     // Create Window
     GLFWwindow *window = glfwCreateWindow(800, 600, "Marama", nullptr, nullptr);
