7653781d1338bfc2981ec7f52a520eafc091fc4e
Shredemption <stijnmichiels2001@gmail.com>
2025-03-06 16:54:59 +0100

    Send bone data to shader in render class

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -83,7 +83,14 @@ void Render::renderSceneModels(Scene &scene, glm::vec4 clipPlane)
 
         shader.setVec4("location_plane", clipPlane);
 
-        renderModel(model.model);
+        if (model.model->boneHierarchy.size() > 0)
+        {
+            model.model->updateBoneTransforms();
+            shader.setMat4Array("u_boneTransforms", model.model->boneTransforms);
+            shader.setMat4Array("inverse_offset", model.model->boneInverseOffsets);
+        }
+
+        renderModel(model);
     }
 }
 
@@ -167,11 +174,12 @@ void Render::renderSceneSkyBox(Scene &scene)
     glBindVertexArray(0);
 }
 
-void Render::renderModel(Model *model)
+void Render::renderModel(ModelData model)
 {
-    for (unsigned int i = 0; i < model->meshes.size(); i++)
-
-        Render::renderMesh(model->meshes[i]);
+    for (unsigned int i = 0; i < model.model->meshes.size(); i++)
+    {
+        Render::renderMesh(model.model->meshes[i]);
+    }
 }
 
 // Render mesh
