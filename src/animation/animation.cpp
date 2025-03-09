86238b2106edd2bc1b0b6c3fe04592f4e2a643a9
Shredemption <stijnmichiels2001@gmail.com>
2025-03-09 11:59:43 +0100

    Turn Frontwheel and add yacht type check

--- src/animation/animation.cpp
+++ src/animation/animation.cpp
@@ -19,6 +19,11 @@ void Animation::generateYachtBones(Model *model)
 {
     wheelAngle += 2.5f;
 
+    if (model->path.find("duvel/duvel") != std::string::npos) {
+        std::string yacht = "duvel";
+    };
+
+    model->boneHierarchy["Armature_Wheel_Front"]->transform = glm::rotate(glm::mat4(1.0f), glm::radians(wheelAngle), glm::vec3(0.0f, 1.0f, 0.0f));
     model->boneHierarchy["Armature_Wheel_Left"]->transform = glm::rotate(glm::mat4(1.0f), glm::radians(wheelAngle), glm::vec3(0.0f, 1.0f, 0.0f));
     model->boneHierarchy["Armature_Wheel_Right"]->transform = glm::rotate(glm::mat4(1.0f), glm::radians(-wheelAngle), glm::vec3(0.0f, 1.0f, 0.0f));
 
