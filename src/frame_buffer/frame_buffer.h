90d5bccfc27ab46711c1f1404dbbd1cc3aef8e23
Shredemption <stijnmichiels2001@gmail.com>
2025-03-31 00:15:47 +0200

    Refactor and added comments to Frame Buffer

--- src/frame_buffer/frame_buffer.h
+++ src/frame_buffer/frame_buffer.h
@@ -6,29 +6,36 @@
 class FrameBuffer
 {
 public:
-    unsigned int frameBuffer;
-    unsigned int colorTexture;
-    unsigned int depthTexture;
-    unsigned int depthRender;
-
-    FrameBuffer() : frameBuffer(0), width(0), height(0) {};
-    FrameBuffer(int width, int height);
-
-    static void bindFrameBuffer(FrameBuffer FBO);
-    static void unbindCurrentFrameBuffer();
-
-    static void WaterFrameBuffers();
+    // Global framebuffer variables
     static bool Water;
-
     static unsigned int reflectionFrameBuffer;
     static unsigned int refractionFrameBuffer;
     static FrameBuffer reflectionFBO;
     static FrameBuffer refractionFBO;
 
+    // Local possible framebuffer attributes
+    unsigned int frameBuffer;
+    unsigned int colorTexture;
+    unsigned int depthTexture;
+    unsigned int depthRender;
+
+    // Default empty constructor
+    FrameBuffer() : frameBuffer(0), width(0), height(0) {};
+
+    // Actual contructor
+    FrameBuffer(int width, int height);
+
+
+    static void bindFrameBuffer(FrameBuffer FBO);
+    static void unbindCurrentFrameBuffer();
+    static void WaterFrameBuffers();
+
 private:
+    // Size variables
     int width;
     int height;
 
+    // Create attachments
     int createTextureAttachment();
     int createDepthTextureAttachment();
     int createDepthBufferAttachment();
