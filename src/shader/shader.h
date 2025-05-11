0bb3f61207894173464665b57733a07987d316ea
Shredemption <stijnmichiels2001@gmail.com>
2025-05-11 13:21:18 +0200

    Shader setIntArray

--- src/shader/shader.h
+++ src/shader/shader.h
@@ -24,6 +24,7 @@ public:
 
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
+    void setIntArray(const std::string &name, const int *values, size_t count) const;
     void setFloat(const std::string &name, float value) const;
     void setVec2(const std::string &name, const glm::vec2 &value) const;
     void setVec2(const std::string &name, float x, float y) const;
