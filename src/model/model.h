7c3172c9e57742859c155ce107afdcc7a9080e67
Shredemption <stijnmichiels2001@gmail.com>
2025-05-12 20:20:25 +0200

    Remove combineMesh from header

--- src/model/model.h
+++ src/model/model.h
@@ -112,7 +112,6 @@ private:
     void loadModel(const std::vector<std::string> &lodPaths, std::string shaderName);
     void processNode(aiNode *node, const aiScene *scene, std::string shaderName, std::vector<Mesh> &targetMeshList, Bone *parentBone = nullptr);
     Mesh processMesh(aiMesh *mesh, const aiScene *scene, std::string shaderName, std::map<std::string, Bone *> &boneHierarchy);
-    void combineMeshes(const aiScene *scene, std::string shaderName);
     std::vector<Texture> loadMaterialTexture(aiMaterial *mat, aiTextureType type, std::string typeName);
     std::string findTextureInDirectory(const std::string &directory, const std::string &typeName);
 
