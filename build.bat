dfb91f361685427ed56cf06333a324a96e3a5465
Shredemption <stijnmichiels2001@gmail.com>
2025-06-11 18:39:16 +0200

    Build using MSVC compiler. Added Ultralight dependency

--- build.bat
+++ build.bat
@@ -18,14 +18,23 @@ if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 
 pushd %BUILD_DIR%
 
-conan install .. --output-folder=. --build=missing --settings=build_type=%BUILD_TYPE%
+conan install .. ^
+    --output-folder=. ^
+    --build=missing ^
+    --settings=build_type=%BUILD_TYPE% ^
+    --settings=compiler=msvc ^
+    --settings=compiler.runtime=dynamic ^
+    --settings=compiler.cppstd=20
 
-cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
-cmake --build .
+cmake .. ^
+    -G "Visual Studio 17 2022" ^
+    -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake ^
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+
+cmake --build . --config %BUILD_TYPE%
 
 popd
-
-strip marama.exe
 
 marama.exe
 
