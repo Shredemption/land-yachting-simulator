8dd0c9becf0c60e10fff7cb507d5438143a21e55
Shredemption <stijnmichiels2001@gmail.com>
2025-06-17 11:58:26 +0200

    Settings screen selectors

--- src/ui_manager/widget.cpp
+++ src/ui_manager/widget.cpp
@@ -25,10 +25,10 @@ void Widget::AddChild(std::shared_ptr<Widget> child)
 
 void Button::Render()
 {
-    glm::vec3 activeColor = hover ? hoverColor : color;
+    glm::vec3 currentColor = active ? activeColor : (hover ? hoverColor : color);
 
     Render::renderText(text, pos.x + 0.003f * scale, pos.y + (0.01f + 0.003f) * scale, scale, glm::vec3(0.0f), alpha, TextAlign::Left);
-    Render::renderText(text, pos.x, pos.y + 0.01f * scale, scale, activeColor, alpha, TextAlign::Left);
+    Render::renderText(text, pos.x, pos.y + 0.01f * scale, scale, currentColor, alpha, TextAlign::Left);
 }
 
 void Button::Update()
@@ -38,10 +38,15 @@ void Button::Update()
     float ymin = WindowManager::screenHeight * (pos.y);
     float ymax = WindowManager::screenHeight * (pos.y + size.y);
 
-    if (InputManager::mousePosX > xmin && InputManager::mousePosX < xmax && InputManager::mousePosY > ymin && InputManager::mousePosY < ymax)
+    if (linkedPage == SceneManager::settingsPage && linkedPage != SettingsPage::None)
+        active = true;
+    else if (InputManager::mousePosX > xmin && InputManager::mousePosX < xmax && InputManager::mousePosY > ymin && InputManager::mousePosY < ymax)
         hover = true;
     else
+    {
+        active = false;
         hover = false;
+    }
 
     if (hover && InputManager::leftMouseButton.released())
         onClick();
