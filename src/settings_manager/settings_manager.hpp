590410e0cf78c74e02e0b5fbff8ac22f4c422b95
Shredemption <stijnmichiels2001@gmail.com>
2025-09-11 13:58:41 +0200

    Split other settings Metadata into struct

--- src/settings_manager/settings_manager.hpp
+++ src/settings_manager/settings_manager.hpp
@@ -7,7 +7,7 @@
 namespace SettingsManager
 {
     inline SettingsStruct settings;
-    inline LimitsStruct limits;
+    inline SettingsMetaStruct settingsMeta;
     inline std::string settingsFile = "settings.json";
 
     void load();
